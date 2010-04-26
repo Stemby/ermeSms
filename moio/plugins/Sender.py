f860737d4ffc5314ce201557d588eb385a51ac0a
Carlo Stemberger <carlo.stemberger@gmail.com>
2010-04-27 00:06:02 +0200

    assigned new substitutes for special characters

--- moio/plugins/Sender.py
+++ moio/plugins/Sender.py
@@ -51,7 +51,15 @@ class Sender(Plugin):
 
     def replaceNonAscii(sender, text):
         """Modifica alcuni caratteri non-ASCII dalla stringa."""
-        replace_map = ((u"à", "a'"), (u"á", "a'"), (u"è", "e'"), (u"é", "e'"), (u"í", "i'"), (u"ì", "i'"), (u"ó", "o'"), (u"ò", "o'"), (u"ú", "u'"), (u"ù", "u'"), (u"À", "A'"), (u"È", "E'"), (u"É", "E'"), (u"Ì", "I'"), (u"Ò", "O'"), (u"Ù", "U'"), (u"ç", "c"), (u"[", "("), (u"]", ")"), (u"{", "("), (u"}", ")"), (u"^", ""), (u"£", "L."), (u"\\", ""), (u"§", ""), (u"°", ""), (u"€", "Euro"))
+        replace_map = (
+                (u"à", "a'"), (u"á", "a'"), (u"è", "e'"), (u"é", "e'"),
+                (u"í", "i'"), (u"ì", "i'"), (u"ó", "o'"), (u"ò", "o'"),
+                (u"ú", "u'"), (u"ù", "u'"), (u"À", "A'"), (u"È", "E'"),
+                (u"É", "E'"), (u"Ì", "I'"), (u"Ò", "O'"), (u"Ù", "U'"),
+                (u"ç", "c"), (u"[", "("), (u"]", ")"), (u"{", "("),
+                (u"}", ")"), (u"^", ""), (u"£", "L."), (u"\\", "/"),
+                (u"|", "/"), (u"§", "Par."), (u"°", "o"), (u"€", "EUR"),
+                (u"~", "Tilde"), (u"`", "'"))
         for old, new in replace_map: text = text.replace(old, new)
         return text
 
