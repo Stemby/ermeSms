08d7df525363bb44f60e9f03d6b7468a31ce9c2f
Thomas Bertani <sylar@anche.no>
2010-05-01 15:09:11 +0200

    trying to fix rossoalice sender (yes, again³)

--- moio/plugins/captchadecoders/AskUserCaptchaDecoder.py
+++ moio/plugins/captchadecoders/AskUserCaptchaDecoder.py
@@ -19,11 +19,10 @@ class AskUserCaptchaDecoder(CaptchaDecoder):
     def decodeCaptcha(self, stream, sender):
         """Questo metodo ritorna la stringa corrispondente all'immagine
         leggibile dallo stream specificato."""
-        print "mmmmmmh"
         GraphicalUI.getInstance().MainFrame.userDecodeCaptchaRequest(stream)
-        print "wtfo?"
         result = GraphicalUI.getInstance().MainFrame.qCaptcha.get()
-        print "ah ma è quello della Lola!"
-        if result == None: raise CaptchaError()
+        if result == None:
+            print "azzz"
+            raise CaptchaError()
         else:
             return result
